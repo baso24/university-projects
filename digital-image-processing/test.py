7eee57dbd25eaff9275b94114d5bb29b827a25c2
Valentino <valentinobasili2424@gmail.com>
2026-02-11 23:08:43 +0100

    nuovo training yolo (v7)

--- digital-image-processing/test.py
+++ digital-image-processing/test.py
@@ -198,7 +198,7 @@ def fall_detection(centroids):
 if __name__ == "__main__":
     
     # Path al modello che si desidera utilizzare per il test
-    MODEL_PATH = 'runs/segment/body_parts4/weights/best.pt' 
+    MODEL_PATH = 'runs/segment/body_parts7/weights/best.pt' 
     
     # Path per il test random, immagine presa dal dataset di validazione
     VAL_IMAGES_PATH = 'assets/cihp-DatasetNinja/processed/images/val'
