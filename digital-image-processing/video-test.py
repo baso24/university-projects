ce5d0933b394086174988538b40801e647f792f7
Valentino <valentinobasili2424@gmail.com>
2026-03-16 19:36:06 +0100

    nuovo sistema di falll detection basato sulla rivelazione di anomalie tramite modello probabilistico delle posizioni dei versori che collegano le parti del corpo

--- digital-image-processing/video-test.py
+++ digital-image-processing/video-test.py
@@ -4,6 +4,140 @@ from ultralytics import YOLO
 import time
 import os
 
+import numpy as np
+from collections import deque
+
+# Classe per rilevare anomalie posturali basate sui versori normalizzati tra i centroidi delle perti del corpo rilevate
+# Si prendono in considerazione 3 versori: Testa->Busto, Testa->Gambe, Busto->Gambe
+# Durante la fase di calibrazione si costruisce media e covarianza di questi vettori in condizioni di normalità (postura eretta)
+# Finita la calibrazione si misura la distanza di Mahalanobis per il numero di coppie di centroidi che vengono rilevati nel frame
+# Se la distanza supera una certa soglia si marca il frame come sospetto
+# Se in una finestra temporale di frames consecuitivi la maggioranza dei frame è sospetta allora si segnala una possibile caduta
+class PosturalAnomalyDetector:
+    # baseline_frames: numero di frame richiesti per costruire la baseline inizial, devono essere presenti tutte e 6 le componenti dei versori
+    # anomaly_thresh: soglia di distanza di Mahalanobis normalizzata per considerare un frame come anomalo (2.0 uquivale a 2 deviazioni standard)
+    # time_window: numero di frame consecutivi da considerare per la finestra di rilevamento caduta
+    def __init__(self, calibration_frames, anomaly_thresh, time_window):
+        self.calibration_frames = calibration_frames
+        self.anomaly_thresh = anomaly_thresh
+        # buffer circolare per tenere traccia degli ultimi N frame per rilevare sequenze di anomalie
+        self.anomaly_buffer = deque(maxlen=time_window)
+        
+        self.calibration_features = [] # Lista per accumulare i vettori di feature durante la fase di calibrazione
+        self.mean = None # Vettore medio dei feature calcolato durante la calibrazione
+        self.cov = None # Matrice di covarianza dei feature calcolata durante la calibrazione
+        self.is_calibrated = False # Flag per indicare se la calibrazione è stata completata
+
+    def extract_features(self, centroids):
+        features = []
+        
+        # Versore Testa (0) -> Busto (2)
+        if 0 in centroids and 2 in centroids:
+            distance = np.array(centroids[2]) - np.array(centroids[0])
+            norm = np.linalg.norm(distance)
+            verser = distance / norm
+            features.extend(verser.tolist())
+        else:
+            features.extend([np.nan, np.nan]) # Uso NaN per i dati mancanti
+        
+        # Versore Testa (0) -> Gambe (3)
+        if 0 in centroids and 3 in centroids:
+            distance = np.array(centroids[3]) - np.array(centroids[0])
+            norm = np.linalg.norm(distance)
+            versor = distance / norm
+            features.extend(versor.tolist())
+        else:
+            features.extend([np.nan, np.nan])
+
+        # Versore Busto (2) -> Gambe (3)
+        if 2 in centroids and 3 in centroids:
+            distance = np.array(centroids[3]) - np.array(centroids[2])
+            norm = np.linalg.norm(distance)
+            versor = distance / norm
+            features.extend(versor.tolist())
+        else:
+            features.extend([np.nan, np.nan])
+            
+        return np.array(features)
+
+    def process_frame(self, centroids):
+        feat = self.extract_features(centroids)
+        
+        # Tolgo i NaN per contare quante coppie di centroidi validi abbiamo (ogni coppia contribuisce con 2 feature, x e y)
+        valid_mask = ~np.isnan(feat)
+        # Conta quante coppie di centroidi validi abbiamo
+        valid_count = np.sum(valid_mask)
+        
+        # Se non c'è nessuna coppia di detection utile, saltiamo il frame
+        if valid_count == 0:
+            self.anomaly_buffer.append(False) # Consideriamo il frame come "non anomalo" per non falsare la finestra temporale, ma in realtà è un dato mancante
+            status = "DATI INSUFFICIENTI"
+            return False, status, 0.0
+
+        # Fase di calibrazione
+        # Esigiamo frame "perfetti" (tutte e 6 le feature) solo per la calibrazione.
+        if not self.is_calibrated:
+            if valid_count == 6:
+                self.calibration_features.append(feat)
+                
+                if len(self.calibration_features) >= self.calibration_frames:
+                    X = np.array(self.calibration_features)
+                    self.mean = np.mean(X, axis=0)
+                    self.cov = np.cov(X, rowvar=False)
+                    # self.cov += np.eye(self.cov.shape[0]) * 1e-4 # Regolarizzazione
+                    
+                    self.is_calibrated = True
+                    
+                    np.set_printoptions(precision=4, suppress=True, linewidth=120)
+                    print("\n" + "="*50)
+                    print(">>> CALIBRAZIONE COMPLETATA <<<")
+                    print(f"Media:\n{self.mean}\nCovarianza:\n{self.cov}")
+                    print("="*50 + "\n")
+                    
+                return False, f"CALIBRAZIONE ({len(self.calibration_features)}/{self.calibration_frames})", 0.0
+            else:
+                return False, "ATTESA FRAME COMPLETO PER CALIBRAZIONE", 0.0
+
+        # Fase di inferenza (la calibrazione è stata completata, possiamo valutare l'anomalia anche su frame con feature parziali)
+        valid_indices = np.where(valid_mask)[0]
+        
+        # Estraiamo le sotto-strutture basandoci SOLO sulle feature visibili
+        feat_sub = feat[valid_indices]
+        mean_sub = self.mean[valid_indices]
+        
+        # np.ix_ permette di estrarre la sottomatrice incrociando righe e colonne valide
+        # In pratica in base ai versori che abbiamo ottenuto dalle classi che stiamo segmenentando nel frame corrente
+        # Costuiamo una matrice di covarianza "ridotta" che considera solo queste classi attive
+        cov_sub = self.cov[np.ix_(valid_indices, valid_indices)]
+        
+        # Inversa della matrice di covarianza ridotta, necessaria per il calcolo della distanza di Mahalanobis
+        cov_sub_inv = np.linalg.inv(cov_sub)
+
+        # Calcolo della distanza di Mahalanobis solo sulle feature valide
+        mean_diff = feat_sub - mean_sub
+        mahalanobis_distance_squared = np.dot(np.dot(mean_diff, cov_sub_inv), mean_diff.T)
+        
+        # Normalizziamo la distanza per il numero di gradi di libertà attivi
+        mahalanobis_distance_normalized = np.sqrt(mahalanobis_distance_squared / valid_count)
+
+        # Un frame è considerato anomalo se la distanza di Mahalanobis normalizzata supera la soglia definita
+        is_anomalous = mahalanobis_distance_normalized > self.anomaly_thresh
+        self.anomaly_buffer.append(is_anomalous)
+
+        # Contiamo quante anomalie ci sono nella finestra temporale e se superano una certa percentuale (es. 70%) allora segnaliamo una possibile caduta
+        anomalies_in_window = sum(self.anomaly_buffer)
+        fall_detected = anomalies_in_window >= (self.anomaly_buffer.maxlen * 0.7)
+
+        if fall_detected:
+            status = "CADUTA RILEVATA!"
+        elif is_anomalous:
+            status = f"POSIZIONE SOSPETTA"
+        else:
+            status = f"POSIZIONE NORMALE"
+
+        return fall_detected, status, mahalanobis_distance_normalized
+
+# Logica di fal detection molto semplice usata in precedenza
 def is_fallen(p1, p2):
     # Se la distanza verticale è minore di quella orizzontale -> Fall detection
     return abs(p1[1] - p2[1]) < abs(p1[0] - p2[0])
@@ -46,6 +180,9 @@ def run_video(model_path, video_path, conf_threshold):
     }
 
     cv2.namedWindow('YOLO Video Fall Detection', cv2.WINDOW_NORMAL)
+
+    # 100 frame di calibrazione a ~30fps sono circa 3 secondi di "postura normale"
+    anomaly_detector = PosturalAnomalyDetector(baseline_frames=100, anomaly_thresh=2, time_window=15)
 
     while True:
         ret, frame = cap.read()
@@ -95,7 +232,6 @@ def run_video(model_path, video_path, conf_threshold):
 
         # Disegno la BBox del tracking movimento per debug visuale
         cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 2, lineType=cv2.LINE_AA)
-        cv2.putText(frame, "Motion ROI", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
         # --- 2. INFERENZA YOLO (SOLO SULLA ROI) ---
         # N.B. YOLO riceve solo la porzione ritagliata, molto più piccola e priva di background
@@ -148,7 +284,8 @@ def run_video(model_path, video_path, conf_threshold):
             cv2.circle(frame, pt, 6, (0, 0, 0), -1)    
             cv2.circle(frame, pt, 4, (255, 0, 0), -1)  
 
-        # --- LOGICA FALL DETECTION ---
+        # --- VECCHIA LOGICA FALL DETECTION ---
+        """"
         fall_detected = False
         pair_found = False
 
@@ -176,8 +313,27 @@ def run_video(model_path, video_path, conf_threshold):
             status_color = (0, 255, 255)
 
         cv2.putText(frame, status_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, status_color, 2)
+        """""
 
-        # --- 3. CALCOLO E STAMPA FPS ---
+        # --- LOGICA FALL DETECTION (ANOMALY DETECTION) ---
+        fall_detected, status_text, mahalanobis_distance_score = anomaly_detector.process_frame(final_centroids)
+
+        if fall_detected:
+            status_color = (0, 0, 255) # Rosso
+        elif "SOSPETTA" in status_text:
+            status_color = (0, 165, 255) # Arancione
+        elif "CALIBRAZIONE" in status_text or "INSUFFICIENTI" in status_text:
+            status_color = (0, 255, 255) # Giallo
+        else:
+            status_color = (0, 255, 0) # Verde
+
+        cv2.putText(frame, status_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, status_color, 2)
+
+        # Stampa del valore della distanza di Mahalanobis normalizzata dopo che è stata fatta la calibrazione
+        if anomaly_detector.is_calibrated:
+            cv2.putText(frame, f"Anomaly Score: {mahalanobis_distance_score:.2f}", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+        # --- CALCOLO FPS ---
         curr_time = time.time()
         # Tempo trascorso in secondi per compiere un iterazione intera
         time_diff = curr_time - prev_time 
@@ -202,6 +358,7 @@ def run_video(model_path, video_path, conf_threshold):
 
 if __name__ == "__main__":
     MODEL_PATH = 'runs/segment/body_parts12/weights/best.pt'
-    VIDEO_PATH_3 = 'digital-image-processing/test-dataset/videos/video_caduta_3.avi'
-    CONF_THRESHOLD = 0.4
-    run_video(MODEL_PATH, VIDEO_PATH_3, CONF_THRESHOLD)
+    VIDEO_PATH_1 = 'digital-image-processing/test-dataset/videos/video_caduta_1.avi'
+
+    CONF_THRESHOLD = 0.2
+    run_video(MODEL_PATH, VIDEO_PATH_1, CONF_THRESHOLD)
